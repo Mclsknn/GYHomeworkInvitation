73f34696e456ed47a33dc254bcea0c6ddb36b45e
Mustafa Çalışkan <mustafa.clsknn@gmail.com>
2022-03-23 15:41:34 +0300

    Add project files.

---
+++ GYHomeworkInvitation/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using GYHomeworkInvitation.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GYHomeworkInvitation.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Register() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Register(Guest guest)
+        {
+            if (ModelState.IsValid)
+            {
+                if (guest.WillCome)
+                {
+                    ViewBag.Come = "Partimize katılacağınız için teşekkür ederiz...";  
+                }
+                else
+                {
+                    ViewBag.Come = "Partimize katılmanızı çok isterdik yinede teşekkür ederiz...";
+                 
+                }
+                return View("LastPage");
+            }
+
+            return View();
+           
+        }
+        public IActionResult LastPage() 
+        {
+            return View();
+        }
+    }
+}
---
+++ GYHomeworkInvitation/Models/Guest.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GYHomeworkInvitation.Models
+{
+    public class Guest
+    {
+        [Required(ErrorMessage ="İsim boş geçilemez")]
+        [MinLength(2,ErrorMessage ="İsim 2 karakterden az olmamalıdır")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Soyisim boş geçilemez")]
+        [MinLength(2, ErrorMessage = "Soyisim 2 karakterden az olmamalıdır")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage ="Email boş geçilemez")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        
+        [DataType(DataType.PhoneNumber)]
+        [RegularExpression(@"^(05(\d{9}))$", ErrorMessage = "Geçersiz telefon numarası")]
+        public string PhoneNumber { get; set; }
+        public bool WillCome { get; set; }
+    }
+}
